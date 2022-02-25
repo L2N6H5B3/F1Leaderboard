a29b9c49fd81faa8e16aa1d761698c48daac41d7
L2N6H5B3 <lbrad@outlook.com>
2022-02-25 12:36:04 +1030

    Add multiple rounds

--- site.js
+++ site.js
@@ -1,6 +1,10 @@
 // Set the location of your CSV file
-var csvLocation = "https://testf1saacc.blob.core.windows.net/csv/test.csv";
+var csvLocation = "<url-to-csv>";
 
+// Set Global variables
+var addedRounds = [];
+var addedDrivers = [];
+var addedPositions = [];
 
 // Get CSV file
 function GetCsv() {
@@ -16,20 +20,80 @@ function GetCsv() {
 function ProcessData(data) {
     // Convert to Objects
     var csvObjects = $.csv.toObjects(data);
+    // Sort by Rounds
+    csvObjects.sort(CompareRounds);
+
+    // Iterate through Objects to add Rounds
+    for (let i = 0; i < csvObjects.length; i++) {
+        if (!addedRounds.includes(csvObjects[i].Round)) {
+            // Add Round
+            AddRound(csvObjects[i].Round);
+            // Add the Round to the Array
+            addedRounds.push(csvObjects[i].Round);
+        }
+    }
+
     // Sort by most to least points
-    csvObjects.sort( ComparePoints );
+    csvObjects.sort(ComparePoints);
 
     // Iterate through Drivers
     for (let i = 0; i < csvObjects.length; i++) {
+        // Get the Index of the Driver
+        var addedDriverIndex = addedDrivers.findIndex(function (value) { return value.DriverName == csvObjects[i].Name; });
+        console.log(addedDriverIndex);
+        // If the Driver Doesn't Exist
+        if (addedDriverIndex == -1) {
+            // Add the Driver to the Array
+            addedDrivers.push({ DriverName: csvObjects[i].Name, DriverTeam: csvObjects[i].Team, DriverPoints: csvObjects[i].Points });
+        } else {
+            console.log("Adding Points to "+csvObjects[i].Name+": "+csvObjects[i].Points);
+            // Add the Driver Points to the Array
+            addedDrivers[addedDriverIndex].DriverPoints = parseInt(addedDrivers[addedDriverIndex].DriverPoints) + parseInt(csvObjects[i].Points);
+        }
+
+        // Get the Index of the Position Counter for this Round
+        var addedPositionIndex = addedPositions.findIndex(function (value) { return value.Round == csvObjects[i].Round; });
+        // If the Round Doesn't Exist
+        if (addedPositionIndex == -1) {
+            // Add the Position to the Array
+            addedPositions.push({ Round: csvObjects[i].Round, Position: 1 });
+            // Get the Position Index
+            addedPositionIndex = addedPositions.findIndex(function (value) { return value.Round == csvObjects[i].Round; });
+        } else {
+            // Add to Position
+            addedPositions[addedPositionIndex].Position++;
+        }
         // Add Driver
-        AddDriver(i+1, csvObjects[i].Name, csvObjects[i].Team, csvObjects[i].Points)
+        AddDriver(csvObjects[i].Round, addedPositions[addedPositionIndex].Position, csvObjects[i].Name, csvObjects[i].Team, csvObjects[i].Points);
+    }
+
+    // Add the Total Standings Container
+    AddTotal();
+
+    // Iterate through AddedDrivers
+    for (let i = 0; i < addedDrivers.length; i++) {
+        // Add Driver
+        AddDriver(0, i + 1, addedDrivers[i].DriverName, addedDrivers[i].DriverTeam, addedDrivers[i].DriverPoints)
     }
 }
 
-// Add A Driver
-function AddDriver(pos, name, team, pts) {
-    // Create Driver Standing DIV
+// Add Round Container
+function AddRound(number) {
+    var titleDiv = document.createElement('div');
+    titleDiv.innerHTML = "Round " + number;
+    titleDiv.className = "round-number";
 
+    var roundDiv = document.createElement('div');
+    roundDiv.className = "standing-round";
+    roundDiv.id = "standing-round-" + number;
+
+    // Append Round Standing DIV to Standings Container
+    document.getElementById("standings-container").appendChild(titleDiv);
+    document.getElementById("standings-container").appendChild(roundDiv);
+}
+
+// Add A Driver
+function AddDriver(round, pos, name, team, pts) {
     var posDiv = document.createElement('div');
     posDiv.innerHTML = pos;
     posDiv.className = "driver-pos";
@@ -53,11 +117,42 @@ function AddDriver(pos, name, team, pts) {
     containerDiv.appendChild(teamDiv);
     containerDiv.appendChild(ptsDiv);
 
-    // Append Driver Standing DIV to Standings Container
-    document.getElementById("standings-container").appendChild(containerDiv);
+    if (round > 0) {
+        // Append Driver Standing DIV to Round Standing Container
+        document.getElementById("standing-round-" + round).appendChild(containerDiv);
+    } else {
+        // Append Driver Standing DIV to Total Standing Container
+        document.getElementById("standing-total").appendChild(containerDiv);
+    }
 }
 
-// Compare Values
+function AddTotal() {
+    var titleDiv = document.createElement('div');
+    titleDiv.innerHTML = "Total";
+    titleDiv.className = "round-total";
+
+    var roundDiv = document.createElement('div');
+    roundDiv.className = "standing-total";
+    roundDiv.id = "standing-total";
+
+    // Append Round Standing DIV to Standings Container
+    document.getElementById("standings-container").prepend(roundDiv);
+    document.getElementById("standings-container").prepend(titleDiv);
+
+}
+
+// Compare Rounds
+function CompareRounds(a, b) {
+    if (parseInt(a.Round) < parseInt(b.Round)) {
+        return 1;
+    }
+    if (parseInt(a.Round) > parseInt(b.Round)) {
+        return -1;
+    }
+    return 0;
+}
+
+// Compare Points
 function ComparePoints(a, b) {
     if (parseInt(a.Points) < parseInt(b.Points)) {
         return 1;
